267248cd1b4dcc7e55e1f3c3a2f82cc52a481a25
Sergey <ommuts@gmail.com>
2020-03-20 23:14:24 +0500

    Расчет

--- js/script.js
+++ js/script.js
@@ -2,26 +2,23 @@ function render() {
     const arrHumans = [1, 1, 1, 1, 7, 11, 14, 17, 20, 28, 34, 45, 59, 63, 93, 114, 147, 199, 253];
     const arrComparePercent = [];
 
-    let i = 1;
+    let i = 0;
+    let k = 0; // берем с 10 индекса arrComparePercent = []
 
-    let html = "";
     let compare = 0;
     let difference = 0;
     let difference2 = 0;
+    let insCont = 0;
 
     let inputNumber = document.getElementById("input_number").value;
     // let buttonNumber = document.getElementsByClassName("button_number");
 
     for (; i < arrHumans.length; i++) {
         difference = arrHumans[i] - compare;
-        difference2 = 100 / compare * difference;
+        difference2 = Math.floor(100 / compare * difference);
         arrComparePercent.push(difference2);
-        document.write("День № " + i + " колличество зараженных " + arrHumans[i] + "<br>");
+        document.write("День № " + i + " колличество зараженных " + arrHumans[i] + " возросло на " + difference2 + " процентов " + "<br>");
         compare = arrHumans[i];
-    }
-
-    for (let k = 0; k < arrComparePercent.length; k++) {
-        document.write(arrComparePercent[i]);
     }
 
     document.write(" <br>" + "Прогноз на основе предыдущих данных" + "<br><br>");
@@ -29,7 +26,7 @@ function render() {
     humans += resultFunc(humans);
 
 
-    for (; i <= inputNumber; i++) {
+    for (; i <= 55; i++) {
         let y = Math.floor(humans);
         humans += resultFunc(humans);
         document.write("День № " + i + " колличество зараженных " + y + "<br>");
@@ -39,10 +36,15 @@ function render() {
         return humans / 100 * 30;
     }
 
+    function funcResult(x) {
+        return insCont / insCont;
+    }
 
-    document.getElementById('result').innerHTML = html;
+    // document.getElementById('result').innerHTML = html;
 }
 
-document.getElementById('button_number').addEventListener('click', render);
+render();
+
+// document.getElementById('button_number').addEventListener('click', render);
 
 
