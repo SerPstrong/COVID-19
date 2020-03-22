20e2c40fed216525382eacfaeac5225123cd28f9
Sergey <ommuts@gmail.com>
2020-03-22 17:16:19 +0500

    speed

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 function render() {
-    const arrHumans = [1, 1, 1, 1, 7, 11, 14, 17, 20, 28, 34, 45, 59, 63, 93, 114, 147, 199, 253, 306];
+    const arrHumans = [" " , 1, 1, 1, 1, 7, 11, 14, 17, 20, 28, 34, 45, 59, 63, 93, 114, 147, 199, 253, 306];
     const arrComparePercent = [];
 
     let result = document.getElementById("result");
-    let resultCom;
+    let resultCom = "";
 
-    let i = 0;
+    let i = 1;
     let k = 0; // берем с 10 индекса arrComparePercent = []
 
-    let compare = 0;
-    let difference;
-    let difference2;
-    let insCont = 0;
-    let html;
-    let iResult;
+    let compare = 1;
+    let difference = 1;
+    let difference2 = 1;
+    let insCont = 1;
+    // let html;
+    let iResult = 1;
 
-    let inputNumber = document.getElementById("input_number").value;
+    // let inputNumber = document.getElementById("input_number").value;
 
     for (; i < arrHumans.length; i++) {
         difference = arrHumans[i] - compare;
@@ -49,20 +49,18 @@ function render() {
     }
 
     function funcIResult() {
-        if (arrComparePercent[arrComparePercent.length-1] > arrComparePercent[arrComparePercent.length-2]) {
-            return iResult = `<i class="fa fa-arrow-up" aria-hidden="true"></i>`
-        }else if (arrComparePercent[arrComparePercent.length-1] < arrComparePercent[arrComparePercent.length-2]) {
+        if (arrComparePercent[arrComparePercent.length - 1] > arrComparePercent[arrComparePercent.length - 2]) {
+            return iResult = `<i class="fa fa-arrow-up i-fa-speed" aria-hidden="true"></i>`
+        } else if (arrComparePercent[arrComparePercent.length - 1] < arrComparePercent[arrComparePercent.length - 2]) {
             return iResult = `<i class="fa fa-arrow-down" aria-hidden="true"></i>`
-        }else {
+        } else {
             return iResult = `<i class="fa fa-arrow-right" aria-hidden="true"></i>`
         }
     }
-
     // document.getElementById('result').innerHTML = html;
 }
 
 render();
 
 // document.getElementById('button_number').addEventListener('click', render);
-
 
