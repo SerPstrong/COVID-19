fae71e0e339ced866b253c3102388e5993aaa4d5
serpstrong <ommuts@gmail.com>
2020-04-03 01:16:22 +0500

    средний

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
+const arrHumans = [" ", 1, 1, 1, 1, 7, 11, 14, 17, 20, 28, 34, 45, 59, 63, 93, 114, 147, 199, 253, 306, 367, 438, 495, 658, 840, 1036, 1264, 1534, 1836, 2337, 2777, 3548];
+let recovered = 235; //выздоровлений
+let deaths = 30; // смертей
+let sverdlRegion = 38;
+let sverdlRegionRecovered = 1;
+
+let active = arrHumans[arrHumans.length - 1] - recovered - deaths;
+
+const arrComparePercent = [];
+let leftBeforeInfection = 0;
+let result = document.getElementById("result");
+let resultTop = document.getElementById("result_top");
+let resultCom = "";
+
+let i = 1;
+let k = 0; // берем с 10 индекса arrComparePercent = []
+
+let compare = 1;
+let difference = 1;
+let difference2 = 1;
+let insCont = 1;
+// let html;
+let iResult = 1;
+let dif = 0;
+
 function render() {
-    const arrHumans = [" ", 1, 1, 1, 1, 7, 11, 14, 17, 20, 28, 34, 45, 59, 63, 93, 114, 147, 199, 253, 306, 367, 438, 495, 658, 840, 1036, 1264, 1534, 1836, 2337, 2777, 3548];
-    let recovered = 235; //выздоровлений
-    let deaths = 30; // смертей
-    let sverdlRegion = 38;
-    let sverdlRegionRecovered = 1;
-
-    let active = arrHumans[arrHumans.length - 1] - recovered - deaths;
-
-    const arrComparePercent = [];
-    let leftBeforeInfection = 0;
-    let result = document.getElementById("result");
-    let resultTop = document.getElementById("result_top");
-    let resultCom = "";
-
-    let i = 1;
-    let k = 0; // берем с 10 индекса arrComparePercent = []
-
-    let compare = 1;
-    let difference = 1;
-    let difference2 = 1;
-    let insCont = 1;
-    // let html;
-    let iResult = 1;
-    let dif = 0;
-
     // let inputNumber = document.getElementById("input_number").value;
 
     resultCom += `<div class="resBlock result"><div class="span-flex"><span class="color-number color-number_size">ДЕНЬ</span></div><div class="span-flex"><span class="color-number color-number_size">ЗАРАЖЕНИЙ</span></div><div class="span-flex"><span class="color-number color-number_size">%</span></div></div>`;
@@ -88,6 +88,16 @@ function render() {
     // }
     // setInterval(sec, 1000);
 
+    let countHumansSum = 0;
+    let hCount = 0;
+    let countHuSum = 1;
+
+    for (; hCount < 20; hCount++) {
+        countHumansSum += arrComparePercent[arrComparePercent.length - countHuSum];
+        countHuSum++
+    }
+    countHumansSum = Math.floor(countHumansSum / hCount);
+
     function sec() {
         let today = new Date().toLocaleDateString();
         let todayTime = new Date().toLocaleTimeString();
@@ -99,7 +109,7 @@ function render() {
         resultTop.innerHTML = `<div class="resBlock">Сегодня <span class="color-number">${iToday} </span> день <span class="color-number">${today}</span>
 </span> <span class="color-number">${todayTime}</span>
 <br>
-Заражений <span class="color-number">${arrHumans[arrHumans.length - 1]} (+${difference})</span>   <span class="color-number">${iResult} ${difference2}%</span>
+Заражений <span class="color-number">${arrHumans[arrHumans.length - 1]} (+${difference})</span>   <span class="color-number">${iResult} ${difference2}%)</span> средний<span class="color-number">(${countHumansSum}%)</span>
 <br>Выздоровлений <span class="color-number recovered_color">${recovered}</span> 
 <br>Смертей <span class="color-number">${deaths}</span> 
 <br>Cвердловская обл. <span class="color-number">${sverdlRegion}</span> <span class="color-number recovered_color">${sverdlRegionRecovered}</span>
@@ -113,7 +123,5 @@ function render() {
     // setTimeout(sec, 1000);
 }
 
-
 // document.getElementById('button_number').addEventListener('click', render);
 render();
-
