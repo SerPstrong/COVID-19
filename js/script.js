d5dddd2f98cb4cdd5ed92572a2954f53020a772b
serpstrong <ommuts@gmail.com>
2020-05-24 15:40:36 +0500

    24.05.20

--- js/script.js
+++ js/script.js
@@ -4,12 +4,12 @@ const arrHumans = [" ", 1, 1, 1, 1, 7, 11, 14, 17, 20, 28, 34, 45, 59, 63, 93, 1
     42853, 47121, 52763, 57999, 62773, 68622, 74588, 80949, 87147, 93558, 99399,
     106498, 114431, 124054, 134687, 145268, 155370, 165929, 177160, 187859, 198676,
     209688, 221344, 232243, 242271, 252245, 262843, 272043, 281752, 290678, 299941,
-    308705, 317554, 326448, 335882];
-let recovered = 107936; //выздоровлений
-let deaths = 3388; // смертей
-let sverdlRegion = 3614;
-let sverdlRegionRecovered = 1207;
-let sverdlRegionDeaths = 14;
+    308705, 317554, 326448, 335882, 344481];
+let recovered = 113299; //выздоровлений
+let deaths = 3541; // смертей
+let sverdlRegion = 3704;
+let sverdlRegionRecovered = 1226;
+let sverdlRegionDeaths = 16;
 let statWorld = statFunc("world ", 4884954, 334947);
 let stat1 = statFunc("usa ", 1601434, 96007);
 let stat2 = statFunc("brazilian ", 330890, 21048);
